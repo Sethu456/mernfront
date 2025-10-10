3feffd69e9a7c64fe063d6c1c60bdcc8f30c19a4
Sethu456 <133879112+Sethu456@users.noreply.github.com>
2025-10-10 17:06:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Student from "./page/student";
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+<Student />
+    </div>
+  );
+}
+
+
+export default App;
