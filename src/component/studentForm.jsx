660244941d4269d60af1c760154556df56201023
Sethu456 <133879112+Sethu456@users.noreply.github.com>
2025-10-11 10:11:14 +0530

    Update studentForm.jsx

--- src/component/studentForm.jsx
+++ src/component/studentForm.jsx
@@ -178,7 +178,7 @@ const MyForm = ({ editID, setFormSubmitted }) => {
           {" "}
           Date:
           <input
-            type="date"
+            type="datetime-local"
             name="dateOfBirth"
             value={formData.dateOfBirth}
             onChange={handleChange}
@@ -222,5 +222,6 @@ const MyForm = ({ editID, setFormSubmitted }) => {
 };
 
 export default MyForm;
+
 
 
